5bd6d3365e1ce603b40164535b7becc2ed27d467
Ollie R <olivernrylance@gmail.com>
2025-07-20 21:04:21 +0100

    Create a basic manual test space

---
+++ manualTests/1/test.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import os
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, root_dir)
+
+from clozeFlashcardGenerator import main
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    # Configure logging
+    logging.basicConfig(
+        level=logging.DEBUG, # Options are DEBUG, INFO, WARNING, ERROR, CRITICAL
+        format='%(levelname)s: %(message)s'
+    )
+
+    inputFilePath: str = 'manualTests/1/sentences.txt'
+    outputFilePath: str = 'manualTests/1/clozeFlashcards.json'
+    clozeChoosingAlgorithm: str = "mostDifferent"
+    n: int = 3
+    benefitShorterSentences: bool = False
+    outputOrder: str = "alphabetical"
+    main(
+        inputFilePath, outputFilePath, clozeChoosingAlgorithm, 
+        n, benefitShorterSentences
+    )

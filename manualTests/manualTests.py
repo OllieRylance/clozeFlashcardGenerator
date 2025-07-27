37ab7d1c92a854af1f69e0e6683511cade5a928a
Ollie R <olivernrylance@gmail.com>
2025-07-27 18:12:09 +0100

    change manual tests to use configs

--- manualTests/manualTests.py
+++ manualTests/manualTests.py
@@ -1,38 +1,14 @@
 import logging
 import sys
 import os
-from typing import Dict, List, Optional
+from typing import List
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, root_dir)
 
-from main import main, OutputOrder, ClozeChoosingAlgorithm
+from main import main
 
 logger = logging.getLogger(__name__)
-
-def runTest(config: Dict[str, str]) -> None:
-    """
-    Run the test with the given config.
-    
-    :param config: A dictionary containing the test configuration.
-    """
-    currentTestFileStart: str = config["FileStart"]
-    inputFilePath: str = f'manualTests/{currentTestFileStart}sentences.txt'
-    outputFilePath: str = f'manualTests/{currentTestFileStart}clozeFlashcards.json'
-    clozeChoosingAlgorithm: ClozeChoosingAlgorithm = ClozeChoosingAlgorithm[config["Algorithm"].upper()]
-    n: int = int(config["n"])
-    benefitShorterSentences: bool = config.get("BenefitShorterSentences") == "True"
-    outputOrderStrings: List[str] = config["OutputOrder"].split(", ")
-    outputOrder: List[OutputOrder] = [
-        OutputOrder[order.strip().upper()] for order in outputOrderStrings
-    ]
-    buryWords: Optional[List[str]] = None
-    if config.get("BuryWords") is not None:
-        buryWords: Optional[List[str]] = config["BuryWords"].split(",")
-    main(
-        inputFilePath, outputFilePath, clozeChoosingAlgorithm, 
-        n, benefitShorterSentences, outputOrder, buryWords
-    )
 
 if __name__ == "__main__":
     # Configure logging
@@ -41,99 +17,23 @@ if __name__ == "__main__":
         format='%(levelname)s: %(message)s'
     )
 
-    configs: List[Dict[str, str]] = [
-        {
-            "FileStart": "alonePunctuationMidCloze/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "punctuationBeforeFirstWord/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "alonePunctuationBeforeFirstWord/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "afterAndAlonePunctuationAfterLastWord/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "doubleAfterPunctuation/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "aloneAndBeforePunctuationMidSentence/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "complexExistingSentenceParsedAndNotDupicatedByNewSentence/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "multiWordExpressionInDifferentOrderGetGrouped/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "sortUnusedFirstThenAlphabetical/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "LEAST_USED_AS_CLOZE_FIRST, ALPHABETICAL"
-        },
-        {
-            "FileStart": "invalidPunctuation/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "wordlessSentence/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "doubleSpace/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL"
-        },
-        {
-            "FileStart": "sortUnusedFirstThenFrequency/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "LEAST_USED_AS_CLOZE_FIRST, FREQUENCY"
-        },
-        {
-            "FileStart": "leastInUsedSentenceFirst/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "LEAST_IN_USED_SENTENCES_FIRST, ALPHABETICAL"
-        },
-        {
-            "FileStart": "buryWord/",
-            "Algorithm": "MOST_DIFFERENT",
-            "n": "3",
-            "OutputOrder": "ALPHABETICAL",
-            "BuryWords": "b"
-        }
+    configFilePaths: List[str] = [
+        "manualTests/afterAndAlonePunctuationAfterLastWord/config.json",
+        "manualTests/aloneAndBeforePunctuationMidSentence/config.json",
+        "manualTests/alonePunctuationBeforeFirstWord/config.json",
+        "manualTests/alonePunctuationMidCloze/config.json",
+        "manualTests/buryWord/config.json",
+        "manualTests/complexExistingSentenceParsedAndNotDupicatedByNewSentence/config.json",
+        "manualTests/doubleAfterPunctuation/config.json",
+        "manualTests/doubleSpace/config.json",
+        "manualTests/invalidPunctuation/config.json",
+        "manualTests/leastInUsedSentenceFirst/config.json",
+        "manualTests/multiWordExpressionInDifferentOrderGetGrouped/config.json",
+        "manualTests/punctuationBeforeFirstWord/config.json",
+        "manualTests/sortUnusedFirstThenAlphabetical/config.json",
+        "manualTests/sortUnusedFirstThenFrequency/config.json",
+        "manualTests/wordlessSentence/config.json"
     ]
 
-    for config in configs:
-        runTest(config)
+    for configFilePath in configFilePaths:
+        main(configFilePath)
